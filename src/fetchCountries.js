22fa337c12fc61563c92aaf7d8aa1993f0aefeda
Abdrii Borinskij <aborinskij@gmail.com>
2022-09-21 22:20:22 +0300

    finish

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,1 +1,11 @@
-import {fetchCountries.js} from
+const BASE_URL = "https://restcountries.com/v3.1/name"
+
+export const fetchCountries = (url) => {
+    return fetch(`${BASE_URL}/${url}?fields=name,capital,population,flags,languages`)
+        .then(response => {
+            if (!response.ok) {
+            throw new Error("No data Loading")
+            } 
+            return response.json()
+    })
+}
--- src/index.js
+++ src/index.js
@@ -1,71 +1,57 @@
 import './css/styles.css';
 import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import {fetchCountries} from './fetchCountries'
+
 const DEBOUNCE_DELAY = 800;
 const inputRef = document.getElementById('search-box');
 const markupRef = document.querySelector(".country-info")
+
 inputRef.addEventListener('input', debounce(getCounrtyData, DEBOUNCE_DELAY))
 
-const BASE_URL = "https://restcountries.com/v3.1/name"
-
-
-
-
+let cauntryName;
 function getCounrtyData(event) {
     const cauntryName = event.target.value.trim();
     if (!cauntryName) {
-        return
+        return markupRef.innerHTML=''
     }
-
     fetchCountries(cauntryName)
         .then(data => {
             if (data.length === 1) { markupCoutry(data[0]) }
-            markupCoutries(data)
+            else if (data.length > 1 && data.length <= 10) { markupCoutries(data) }
+            else { Notify.info("Too many matches found. Please enter a more specific name.") }
         })
-        .catch(err =>{ alert(err)})
-
+        .catch(err => { Notify.failure("Oops, there is no country with that name"), markupRef.innerHTML = '' })
 }
 
 
-
-
-
- const fetchCountries = (url) => {
-    return fetch(`${BASE_URL}/${url}?fields=name,capital,population,flags,languages`)
-        .then(response => {
-            if (!response.ok) {
-            throw new Error("No data Loading")
-            }
-            return response.json()
-    })
-}
 
   
 
 
 function markupCoutries(coutryData) {
-console.log('markupCoutries :', coutryData);
-
-    // `<div>
-// </div>`
-   
+    const markup = coutryData.map(element =>
+    { return`<div><li><img src=${element.flags.svg } alt= flags width= '100'</li><h4>${element.name.official}</h4></div>`;
+    })
+    markupRef.innerHTML = markup;
 } 
 
 
 
 function markupCoutry(coutryData) {
-    let languagesObject = coutryData.languages;
-    console.log('languagesObject :', languagesObject);
-    let languages = Object.values(languagesObject)
-    console.log('lang :', languages);
 
-// console.log('coutryData :', coutryData.languages);
+    let languagesObject = coutryData.languages;
+    let languages = Object.values(languagesObject).join(', ');
+
     markupRef.innerHTML = `<div>
     <h1>${coutryData.name.official}</h1>
-    <h4>Capital: ${coutryData.capital}</h4>
-    <h4>Population: ${coutryData.population}</h4>
-    <h4>Languages: ${languages}</h4>
+    <ul>
+    <li><img src=${coutryData.flags.svg} alt= flags width= '100'</li>
+    <li>Capital: ${coutryData.capital}</li>
+    <li>Population: ${coutryData.population}</li>
+    <li>Languages: ${languages}</li>
+    </ul>
     </div>`;
     
-   
 } 
 
